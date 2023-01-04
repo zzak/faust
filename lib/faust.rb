c06f712d6eeea618faed59f80dc2cb41d82e629b
zzak <zzakscott@gmail.com>
2023-01-04 15:45:42 +0900

    init

---
+++ lib/faust.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "faust/version"
+
+module Faust
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/faust/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Faust
+  VERSION = "0.1.0"
+end
